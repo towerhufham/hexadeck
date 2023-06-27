6395f3a97f16834ea6ba46f519697c7767dd71ed
towerhufham <32599109+towerhufham@users.noreply.github.com>
2023-06-26 21:19:29 -0400

    Initial commit

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="mute1">Mute 1</button>
+  <button @click="mute2">Mute 2</button>
+</template>
+
+<script lang="ts" setup>
+  import {Howl, Howler} from "howler"
+
+  const sound1 = new Howl({
+    src: ["/samples/geo1.wav"],
+    loop: true,
+  })
+  const sound2 = new Howl({
+    src: ["/samples/geo2.wav"],
+    loop: true,
+  })
+  const mute1 = () => {
+    sound1.volume(0)
+  }
+  const mute2 = () => {
+    sound2.volume(0)
+  }
+  onMounted(() => {
+    sound1.play()
+    sound2.play()
+  })
+</script>
